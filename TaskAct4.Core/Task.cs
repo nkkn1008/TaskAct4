fbd1c4425b144ae2fa79e7bdd7db5fba4d19e697
Kenichi Nakamura <nkkn1008@gmail.com>
2016-03-15 21:41:15 +0900

    Modell情報用の Project を新規作成 Task を扱う最小単位のクラスを新規作成

---
+++ TaskAct4.Core/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAct4.Core
+{
+    public class Task
+    {
+        private string _Name;
+        Task(string Name)
+        {
+            _Name = Name;
+        }      
+    }
+}
